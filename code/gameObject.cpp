48dafd29df234a2a414892c9ee4e29927b3f81b9
savageproto1 <155660504+savageproto1@users.noreply.github.com>
2024-01-18 17:37:27 +0000

    modified:   code/gameObject.cpp

--- code/gameObject.cpp
+++ code/gameObject.cpp
@@ -3,10 +3,10 @@
 class Cube{
     public:
 
-        Cube(int X, int Y){
+        Cube(int X, int Y, game* main){
             for(int Y = 0; Y < 5; Y++){
                 for(int X = 0; X < 5; X ++)
-                ;
+                main -> gameField[0][X][Y] = 1;
             }
         }
 };
@@ -19,7 +19,7 @@ class game{
              unsigned short gameField[2][screenWidth][screenHieght];
 
          int cube(int X, int Y){
-            Cube *s = new Cube(X,Y);
+            Cube *s = new Cube(X,Y,this );
             
            
             
